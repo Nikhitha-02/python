58b6db0e0cdf5490d871fe3a3063ab12c67c4987
nikhitha <muninikhithaveluru@gmail.com>
2025-06-30 08:34:42 +0530

    Adding 1.max_product_of_Three.py 2.min_perimeter_rectangle.py

---
+++ min_perimeter_rectangle.py
@@ -0,0 +1,34 @@
+"""
+An integer N is given, representing the area of some rectangle.
+The area of a rectangle whose sides are of length A and B is A * B, and the perimeter is 2 * (A + B).
+The goal is to find the minimal perimeter of any rectangle whose area equals N. The sides of this rectangle should be only integers.
+
+For example, given integer N = 30, rectangles of area 30 are:
+(1, 30), with a perimeter of 62,
+(2, 15), with a perimeter of 34,
+(3, 10), with a perimeter of 26,
+(5, 6), with a perimeter of 22.
+Write a function:
+def solution(N)
+
+that, given an integer N, returns the minimal perimeter of any rectangle whose area is exactly equal to N.
+For example, given an integer N = 30, the function should return 22, as explained above.
+"""
+import math
+
+def minPerimeterRectangle(N):
+    """
+    Finds the smallest perimeter of a rectangle with area N and integer side lengths.
+    Args: N (int): The area of the rectangle.
+    Returns: int: The minimum possible perimeter.
+    """
+    min_perimeter=None
+    for i in range(1, int(math.sqrt(N)) + 1):
+        if N % i == 0:
+            j = N // i
+            perimeter= 2 * (i + j)
+            if min_perimeter is None or perimeter < min_perimeter:
+                min_perimeter = perimeter
+    return min_perimeter
+
+print(minPerimeterRectangle(30))
